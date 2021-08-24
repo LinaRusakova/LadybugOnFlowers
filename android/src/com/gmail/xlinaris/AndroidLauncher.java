7f77d9396c22a5919f98da0528d015471063d8c2
Lina Rusakova <xlinaris@gmail.com>
2021-08-25 00:19:04 +0300

    Initial commit

---
+++ android/src/com/gmail/xlinaris/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gmail.xlinaris;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gmail.xlinaris.LadybugOnFlowers;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LadybugOnFlowers(), config);
+	}
+}
