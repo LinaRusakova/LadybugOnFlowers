170b962ae33efbae80ca758074b88af575e43e77
Lina Rusakova <xlinaris@gmail.com>
2021-08-25 03:40:57 +0300

    My homework for lesson 1.

--- core/src/com/gmail/xlinaris/LadybugOnFlowers.java
+++ core/src/com/gmail/xlinaris/LadybugOnFlowers.java
@@ -1,31 +1,33 @@
 package com.gmail.xlinaris;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class LadybugOnFlowers extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture img;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("background1024x1024.png");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(Color.valueOf("#3a90e7"));
+        batch.begin();
+        batch.draw(img, 0, 0, 1024, 1024);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
